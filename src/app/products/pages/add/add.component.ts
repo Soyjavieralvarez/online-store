b6314b47d5d74b5af03f44364010b42ed0f91403
javieralvarez <javier.alvarez.gonzalez@live.com>
2023-04-13 17:38:11 +0200

    form new product done!

--- src/app/products/pages/add/add.component.ts
+++ src/app/products/pages/add/add.component.ts
@@ -1,10 +1,25 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { Product } from '../../interfaces/interfaces';
 
 @Component({
   selector: 'app-add',
   templateUrl: './add.component.html',
   styleUrls: ['./add.component.scss']
 })
-export class AddComponent {
+export class AddComponent implements OnInit{
 
+  product: Product = {
+    name:'',
+    size:'',
+    price:'',
+    color: '',
+    type:'',
+    description:'',
+    alt_img:'',
+  }
+
+  constructor() {}
+  ngOnInit(): void {
+    
+  }
 }
