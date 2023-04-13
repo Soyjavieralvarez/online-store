429508ab70a3dd895451d334008a640282ccc7d9
javieralvarez <javier.alvarez.gonzalez@live.com>
2023-04-13 12:32:30 +0200

    component searchind almost done

--- src/app/products/pages/search/search.component.ts
+++ src/app/products/pages/search/search.component.ts
@@ -1,5 +1,6 @@
 import { Component, OnInit } from '@angular/core';
 import { Product } from '../../interfaces/interfaces';
+import { ProductsService } from '../../services/products.service';
 
 @Component({
   selector: 'app-search',
@@ -9,9 +10,14 @@ import { Product } from '../../interfaces/interfaces';
 export class SearchComponent implements OnInit {
   term: string = '';
   products: Product[] = []
-  constructor() { }
+
+  constructor(private productsService: ProductsService) { }
 
   ngOnInit(): void {
-    
+  }
+
+  searching() {
+    this.productsService.getProducts()
+    .subscribe( products => this.products = products);
   }
 }
