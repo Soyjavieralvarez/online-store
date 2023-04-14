4b4c38c4012314025e982c47cc1b2d38c8876975
javieralvarez <javier.alvarez.gonzalez@live.com>
2023-04-14 20:31:25 +0200

    custom validations

--- src/app/auth/auth.module.ts
+++ src/app/auth/auth.module.ts
@@ -4,17 +4,19 @@ import { LoginComponent } from './pages/login/login.component';
 import { RegisterComponent } from './pages/register/register.component';
 import { AuthRoutingModule } from './auth-routing.module';
 import { MaterialModule } from '../material/material.module';
+import { ReactiveFormsModule } from '@angular/forms';
 
 
 
 @NgModule({
   declarations: [
     LoginComponent,
-    RegisterComponent
+    RegisterComponent,
   ],
   imports: [
     CommonModule,
     AuthRoutingModule,
+    ReactiveFormsModule,
     MaterialModule
   ]
 })
--- src/app/auth/pages/register/register.component.ts
+++ src/app/auth/pages/register/register.component.ts
@@ -1,10 +1,42 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { FormBuilder, FormControl, FormGroup, Validators } from '@angular/forms';
 
 @Component({
   selector: 'app-register',
   templateUrl: './register.component.html',
   styleUrls: ['./register.component.scss']
 })
-export class RegisterComponent {
+export class RegisterComponent implements OnInit {
+
+  fullNamePattern: string = "([a-zA-Z]+) ([a-zA-Z]+)";
+  emailPattern: string = "^[a-z0-9._%+-]+@[a-z0-9.-]+\\.[a-z]{2,4}$";
+
+  cantBeStrider(control: FormControl ) {
+    console.log(control.value)
+  }
+
+  myForm: FormGroup = this.fb.group({
+    name:['', [ Validators.required, Validators.pattern(this.fullNamePattern) ]],
+    email:['', [ Validators.required, Validators.pattern( this.emailPattern )]]
+  })
+
+constructor ( private fb: FormBuilder ) {}
+
+ngOnInit(): void{
+  this.myForm.reset({
+    name:'Javier Álvarez',
+    email:'javier@javier.com'
+  })
+}
+
+invalidField( field:string ) {
+  return this.myForm.get(field)?.invalid
+  && this.myForm.get(field)?.touched
+}
+
+submitForm() {
+  console.log(this.myForm.value);
+  this.myForm.markAllAsTouched();
+}
 
 }
